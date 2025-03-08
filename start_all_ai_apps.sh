268167acd33b7f03d62a7af22b993a652d72b75f
Scott Mutchler <smutchle@vt.edu>
2025-03-07 20:37:57 -0500

    changes

---
+++ start_all_ai_apps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+AI_BASE_DIR=`pwd`
+CURRENT_DIR=`pwd`
+
+cd $AI_BASE_DIR/references_bot/
+./run_references_app_in_background.sh
+
+cd $AI_BASE_DIR/research_rag_bot
+./run_chatbot_in_background.sh
+
+cd $AI_BASE_DIR/web_researcher_bot
+./run_researcher_in_background.sh
+
+cd $CURRENT_DIR
